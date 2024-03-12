bb2512e7db34e75fea7bc89664cd630ae2784433
Carlos Tomás <carlos.tomas@marionete.co.uk>
2024-03-12 17:15:26 +0100

    feat: change

--- main.tf
+++ main.tf
@@ -2,14 +2,14 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.26.0"
+      version = "5.40.0"
     }
     random = {
       source  = "hashicorp/random"
       version = "3.0.1"
     }
   }
-  required_version = "~> 1.1.0"
+  required_version = "~> 1.5.7"
 
   backend "remote" {
     organization = "hevil-terraform-labs"
